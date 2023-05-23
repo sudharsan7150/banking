7684bd7dd98e9c6f867c73df04c97e818c8e7335
sudharsan7150 <sudharsan7150@gmail.com>
2023-05-23 10:54:15 +0530

    initial commit

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ app.use(express.json());
 
 const PORT = 3000;
 
-app.listen(PORT,()=> {
+app.listen(PORT, () => {
     console.log('SERVER  :  http://localhost:${PORT}');
-    connection.connect((err)=>{
+    connection.connect((err) => {
         if (err) throw err;
         console.log("DATABASE CONNECTED");
     })
@@ -30,143 +30,155 @@ app.listen(PORT,()=> {
 app.use(cors());
 
 
-app.use("/all_account", (req, res)=> {
-        const sql_query = 'select * from  Bank'
-        connection.query(sql_query, (err, result)=> {
-        if(err) throw err;
+app.use("/all_account", (req, res) => {
+    const sql_query = 'select * from  Bank'
+    connection.query(sql_query, (err, result) => {
+        if (err) throw err;
         res.send(result);
     })
 })
 
-     app.use("/create_account", (req, res) => {
-        const sql_query = "INSERT INTO Bank (name, accno, accbal,credit) VALUES ('gokul', '108', '400', 'true')";
+app.use("/create_account", (req, res) => {
+    const sql_query = "INSERT INTO Bank (name, accno, accbal,credit) VALUES ('gokul', '108', '400', 'true')";
+    connection.query(sql_query, (err, result) => {
+        if (err) throw err;
+        console.log('account created successfully');
+        res.send(result);
+    })
+})
+
+app.use("/get_account", (req, res) => {
+    console.log(req.url);
+    var accno = 101;
+    var queryData = url.parse(req.url, true).query;
+    console.log(queryData);
+    const sql_query = 'SELECT * FROM Bank WHERE accno = ' + queryData.accno;
+    console.log('SELECT * FROM Bank WHERE accno = ' + queryData.accno);
+    connection.query(sql_query, (err, result) => {
+        if (err) throw err;
+        console.log("")
+        res.send(result);
+
+    })
+})
+
+
+app.use("/get_currentaccbal", (req, res) => {
+    console.log(req.url);
+    var accno = 101;
+    var queryData = url.parse(req.url, true).query;
+    console.log(queryData);
+    const sql_query = 'SELECT name,accbal FROM Bank WHERE accno = ' + queryData.accno;
+    console.log('SELECT accbal FROM Bank WHERE accno = ' + queryData.accno);
+    connection.query(sql_query, (err, result) => {
+        if (err) throw err;
+        console.log("")
+        res.send(result);
+
+    })
+})
+
+
+
+
+
+app.use("/update", (req, res) => {
+    console.log(req.url);
+    var accno = 101;
+    var queryData = url.parse(req.url, true).query;
+    console.log(queryData);
+    const sql_query = 'UPDATE Bank set accbal = ' + queryData.accbal + ' WHERE accno = ' + queryData.accno;
+    console.log('UPDATE Bank set accbal = ' + queryData.accbal + 'WHERE accno = ' + queryData.accno);
+    connection.query(sql_query, (err, result) => {
+        if (err) throw err;
+        console.log("Update unsuccessful");
+        res.send(result);
+    })
+})
+
+
+app.use("/transfer", (req, res) => {
+    console.log(req.url);
+    var queryData = url.parse(req.url, true).query;
+    console.log(queryData);
+    const sql_query = 'SELECT name,accbal FROM Bank WHERE accno = ' + queryData.toAcc;
+    console.log('SELECT accbal FROM Bank WHERE accno = ' + queryData.toAcc);
+    connection.query(sql_query, (err, result) => {
+        if (err) throw err;
+        console.log('result = ', result[0].accbal);
+        console.log('queryData = ', queryData.amount);
+        var new_accbal = result[0].accbal += + queryData.amount;
+        console.log(new_accbal);
+        const sql_query = 'UPDATE Bank set accbal = ' + new_accbal + ' WHERE accno = ' + queryData.toAcc;
+        console.log('UPDATE Bank set accbal = ' + new_accbal + ' WHERE accno = ' + queryData.toAcc);
         connection.query(sql_query, (err, result) => {
             if (err) throw err;
-            console.log('account created successfully');
-            res.send(result);
-  })
-        })
-
-        app.use("/get_account", (req, res) => {
+            //    res.send(result);
             console.log(req.url);
-                var accno = 101 ;
-                var queryData = url.parse(req.url, true).query;
-                console.log(queryData);
-            const sql_query = 'SELECT * FROM Bank WHERE accno = ' + queryData.accno;
-            console.log('SELECT * FROM Bank WHERE accno = ' + queryData.accno);
+            var queryData = url.parse(req.url, true).query;
+            console.log(queryData);
+            const sql_query = 'SELECT name,accbal FROM Bank WHERE accno = ' + queryData.fromAcc;
+            console.log('SELECT accbal FROM Bank WHERE accno = ' + queryData.fromAcc);
+            connection.query(sql_query, (err, result) => {
+                if (err) throw err;
+                console.log("Insufficient balance");
+                console.log('result = ', result[0].accbal);
+                console.log('queryData = ', queryData.amount);
+                var new_accbal = result[0].accbal - queryData.amount;
+                console.log(new_accbal);
+                const sql_query = 'UPDATE Bank set accbal = ' + new_accbal + ' WHERE accno = ' + queryData.fromAcc;
+                console.log('UPDATE Bank set accbal = ' + new_accbal + ' WHERE accno = ' + queryData.fromAcc);
+                connection.query(sql_query, (err, result) => {
+                    if (err) throw err;
+                    res.send(result);
+                   
+                var sql_query = "INSERT INTO transaction_history (fromacc,toacc,amount,date) VALUES ?";
+                const date = new Date();
+
+                 let day = date.getDate();
+                 let month = date.getMonth() + 1;
+                 let year = date.getFullYear();
+
+
+                   let currentDate = `${day}/${month}/${year}`;
+
+                var values = [
+                   [(queryData.fromAcc),(queryData.toAcc),(queryData.amount),(currentDate)]
+                
+                ];
+                connection.query(sql_query, [values], function (err, result){
+                    if (err) throw err;
+                    console.log("Number of records inserted:" + result.affectedRows);
+                })
+
+
+
+
+                    })
+
+                })
+            })
+
+        })
+
+
+    })
+
+
+
+    app.use("/delete_account", (req, res) => {
+        console.log(req.url);
+        var accno = 105;
+        var queryData = url.parse(req.url, true).query;
+        console.log(queryData);
+        const sql_query = 'DELETE FROM Bank WHERE accno = ' + queryData.accno;
+        console.log('DELETE FROM Bank WHERE accno = ' + queryData.accno);
         connection.query(sql_query, (err, result) => {
-             if (err) throw err;
-             console.log("")
+            if (err) throw err;
             res.send(result);
-        
         })
-    })
-
-
-    app.use("/get_currentaccbal", (req, res)=> { 
-        console.log(req.url);
-        var accno = 101 ;
-          var queryData = url.parse(req.url, true).query;
-                console.log(queryData);
-         const sql_query = 'SELECT name,accbal FROM Bank WHERE accno = ' + queryData.accno;
-         console.log('SELECT accbal FROM Bank WHERE accno = ' + queryData.accno);
-       connection.query(sql_query, (err, result) => {
-       if (err) throw err;
-             console.log("")
-            res.send(result);
-        
-        })
-    })
-   
-
-
-
-
-    app.use("/update",(req, res)=>{
-         console.log(req.url);
-                var accno = 101;
-                var queryData = url.parse(req.url, true).query;
-                console.log(queryData);
-         const sql_query = 'UPDATE Bank set accbal = ' + queryData.accbal + ' WHERE accno = ' + queryData.accno;
-    console.log('UPDATE Bank set accbal = ' + queryData.accbal + 'WHERE accno = ' + queryData.accno);
-    connection.query(sql_query, (err, result)=> {
-       if(err) throw err;
-       console.log("Update unsuccessful");
-       res.send(result);
-    })
-})
-     
-
-  app.use("/transfer",(req, res)=>{
-        console.log(req.url);
-          var queryData = url.parse(req.url, true).query;
-                console.log(queryData);
-         const sql_query = 'SELECT name,accbal FROM Bank WHERE accno = ' + queryData.toAcc;
-         console.log('SELECT accbal FROM Bank WHERE accno = ' + queryData.toAcc);
-       connection.query(sql_query, (err, result) => {
-           if (err) throw err;
-           console.log('result = ',result[0].accbal);
-           console.log('queryData = ',queryData.amount);
-           var new_accbal = result[0].accbal +=+ queryData.amount;
-           console.log(new_accbal);
-            const sql_query = 'UPDATE Bank set accbal = ' + new_accbal + ' WHERE accno = ' + queryData.toAcc;
-    console.log('UPDATE Bank set accbal = ' + new_accbal + ' WHERE accno = ' + queryData.toAcc);
-    connection.query(sql_query, (err, result)=> {
-       if(err) throw err;
-        //    res.send(result);
-        console.log(req.url);
-        var queryData = url.parse(req.url, true).query;
-                console.log(queryData);
-         const sql_query = 'SELECT name,accbal FROM Bank WHERE accno = ' + queryData.fromAcc;
-         console.log('SELECT accbal FROM Bank WHERE accno = ' + queryData.fromAcc);
-       connection.query(sql_query, (err, result) => {
-           if (err) throw err;
-           console.log("Insufficient balance");
-           console.log('result = ',result[0].accbal);
-           console.log('queryData = ',queryData.amount);
-            var new_accbal = result[0].accbal - queryData.amount;
-            console.log(new_accbal);
-            const sql_query = 'UPDATE Bank set accbal = ' + new_accbal + ' WHERE accno = ' + queryData.fromAcc;
-    console.log('UPDATE Bank set accbal = ' + new_accbal + ' WHERE accno = ' + queryData.fromAcc);
-    connection.query(sql_query, (err, result)=> {
-        if(err) throw err;
-        res.send(result);
-      
-    
-       })
-    
-    })
-    })
 
     })
-
-
-    //      console.log(req.url);
-    //             var accno = 101;
-    //             var queryData = url.parse(req.url, true).query;
-    //             console.log(queryData);
-    //      const sql_query = 'UPDATE Bank set accbal = ' + queryData.accbal + ' WHERE accno = ' + queryData.accno;
-    // console.log('UPDATE Bank set accbal = ' + queryData.accbal + 'WHERE accno = ' + queryData.accno);
-    // connection.query(sql_query, (err, result)=> {
-    //    if(err) throw err;
-    //    res.send(result);
-    // })
-})
-       
-
-   app.use("/delete_account", (req, res)=> {
-       console.log(req.url);
-       var accno = 105 ;
-        var queryData = url.parse(req.url, true).query;
-       console.log(queryData);
-       const sql_query = 'DELETE FROM Bank WHERE accno = ' + queryData.accno;
-       console.log('DELETE FROM Bank WHERE accno = ' + queryData.accno);
-       connection.query(sql_query, (err, result) => {
-           if (err) throw err;
-           res.send(result);
-       })
-
-   }) 
 
 
 
